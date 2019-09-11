704bff1ef32f4385248fb115efb0113bb9934c56
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-09-11 05:43:40 +0000

    Edit:config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'diaries/new'
-  get 'diaries/edit'
-  get 'diaries/index'
-  get 'diaries/show'
+  resources :diaries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'diaries#index'
 end
