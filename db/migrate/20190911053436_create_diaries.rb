b24f6ae961c83e297f7534042cd4fc2382643afb
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-09-11 05:39:03 +0000

    Validation and Migration

--- db/migrate/20190911053436_create_diaries.rb
+++ db/migrate/20190911053436_create_diaries.rb
@@ -1,7 +1,7 @@
 class CreateDiaries < ActiveRecord::Migration[5.2]
   def change
     create_table :diaries do |t|
-      t.date :date
+      t.date :date, index: { unique: true }
       t.text :body
 
       t.timestamps
