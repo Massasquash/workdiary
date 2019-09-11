820e58c88dfbddda45deb4f6b24428331668a7e8
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-09-11 05:53:44 +0000

    Edit:Create Action

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,6 +2,15 @@ class DiariesController < ApplicationController
   def new
     @diary = Diary.new
   end
+  
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to diaries_path
+    else
+      render 'new'
+    end
+  end
 
   def edit
   end
@@ -11,4 +20,11 @@ class DiariesController < ApplicationController
 
   def show
   end
+
+  private
+  
+  def diary_params
+    params.require(:diary).permit(:date, :body)
+  end
+
 end
