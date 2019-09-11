bcda6287fd792740fc989de6b2f87ebfe1fc8890
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-09-11 05:41:07 +0000

    Create Controller:Diaries

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'diaries/new'
+  get 'diaries/edit'
+  get 'diaries/index'
+  get 'diaries/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
