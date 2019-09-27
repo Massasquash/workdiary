88ee2cffe1707d1a75c9cf315254683c5d3029da
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-09-27 22:49:01 +0000

    Add:Templates Resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :diaries do
     resources :works, only: [:new, :create, :edit, :update, :destroy]
   end
+  
+  resources :templates, only: [:new, :create, :edit, :update, :index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'diaries#index'
 end
