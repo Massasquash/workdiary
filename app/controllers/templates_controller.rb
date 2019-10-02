3a016323e2e1bca0d52eb7c77869db08df2592b3
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-10-02 14:45:52 +0000

    Improve:Works New to Templates New with Using Session

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,12 +1,17 @@
 class TemplatesController < ApplicationController
+  before_action
+  
   def new
     @template = Template.new
+    session[:diary_id] = params[:format]
   end
 
   def create
     @template = Template.new(template_params)
+    diary_id = session[:diary_id]
     if @template.save
-      redirect_to diaries_path
+      redirect_to new_diary_work_path(diary_id)
+      session[:diary_id] = nil
     else
       render 'new'
     end
