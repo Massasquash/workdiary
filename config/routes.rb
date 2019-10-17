86b76746ab21bd3d072faffb505683262c90f00b
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-10-17 03:50:26 +0000

    Correct:Routing Resources Templates in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
     resources :works, only: [:new, :create, :edit, :update, :destroy]
   end
   
-  resources :templates, only: [:new, :create, :edit, :update, :index, :destroy]
+  resources :templates, only: [:new, :create]
   
   get 'templates/get_body'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'diaries#index'
-  
-
   
 end
