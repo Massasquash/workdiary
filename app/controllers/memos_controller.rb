0e29a4f82592ca46de63e9ecae09080e7845eb19
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-10-25 22:20:01 +0000

    Add method create in MemosController

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,5 +5,22 @@ class MemosController < ApplicationController
     @work = @diary.works.find params[:work_id]
     @memo = @work.build_memo
   end
+  
+  def create
+    @diary = Diary.find params[:diary_id]
+    @work = @diary.works.find params[:work_id]
+    @memo = @work.create_memo(memo_params)
+    if @memo.save
+      redirect_to diary_path(@diary)
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+  def memo_params
+    params.require(:memo).permit(:body)
+  end
+  
 end
