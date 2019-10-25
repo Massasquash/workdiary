d8e7bedfffe8051be97bbf1b100ce235adca35cb
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-10-25 13:59:22 +0000

    Add method new in MemosController, Add view memos/new

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,9 @@
 class MemosController < ApplicationController
+
+  def new
+    @diary = Diary.find params[:diary_id]
+    @work = @diary.works.find params[:work_id]
+    @memo = @work.build_memo
+  end
+
 end
