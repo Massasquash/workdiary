969893623e09629ff6349a71a7cb9d6990fb299d
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-11-12 02:38:49 +0000

    Clean val in MemosController ,Add link to creating new memo in diaries/show

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,16 +2,14 @@ class MemosController < ApplicationController
 
   def new
     @work = Work.find params[:work_id]
-    @diary = Diary.find @work.diary_id
     @memo = @work.build_memo
   end
   
   def create
     @work = Work.find params[:work_id]
-    @diary = Diary.find @work.diary_id
     @memo = @work.build_memo(memo_params)
     if @memo.save
-      redirect_to diary_path(@diary)
+      redirect_to diary_path(@work.diary)
     else
       render 'new'
     end
@@ -19,16 +17,14 @@ class MemosController < ApplicationController
   
   def edit
     @work = Work.find params[:work_id]
-    @diary = Diary.find @work.diary_id
     @memo = @work.memo
   end
   
   def update
     @work = Work.find params[:work_id]
-    @diary = Diary.find @work.diary_id
     @memo = @work.memo
     if @memo.update(memo_params)
-      redirect_to diary_path(@diary)
+      redirect_to diary_path(@work.diary)
     else
       render 'edit'
     end
@@ -36,10 +32,9 @@ class MemosController < ApplicationController
   
   def destroy
     work = Work.find params[:work_id]
-    diary = Diary.find work.diary_id
     memo = work.memo
     memo.destroy!
-    redirect_to diary_path(diary)
+    redirect_to diary_path(work.diary)
   end
 
 
