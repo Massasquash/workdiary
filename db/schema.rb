f38b19573330fd9697a20db79b1194bc39901df8
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-11-15 00:57:15 +0000

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_112524) do
+ActiveRecord::Schema.define(version: 2019_11_15_005602) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "diaries", force: :cascade do |t|
     t.date "date"
