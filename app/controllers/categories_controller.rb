2d83dc91aa754479b17c189dbb39e667c01098d0
MasahiroOhsaki <fantasy5512.fun@gmail.com>
2019-11-15 04:20:22 +0000

    Add method create in categories_ciontroller with using session

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,24 @@
 class CategoriesController < ApplicationController
   def new
     @category = Category.new
+    @diary_id = session[:diary_id] = params[:diary_id]
   end
+  
+  def create
+    @category = Category.new(category_params)
+    @diary_id = session[:diary_id]
+    if @category.save
+      redirect_to new_diary_work_path(@diary_id)
+      session[:diary_id] = nil
+    else 
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end  
+  
 end
